b3cf153117ac60e70d20d25f9a08d79c6b08d82c
kucingoren100 <kucingoren100199@gmail.com>
2026-06-30 09:32:28 +0700

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'https://sistem-manajemen-tim-production.up.railway.app/' });
+const api = axios.create({ baseURL: 'https://sistem-manajemen-tim-production.up.railway.app/api' });
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token');
   if (token) cfg.headers.Authorization = `Bearer ${token}`;
